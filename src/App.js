199da8ba217b5e83f26b4322d3027f79fcd8f554
Pranay Aryal <drpranayaryal@gmail.com>
2018-05-21 23:21:41 -0400

    remove default boilterplate

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React, Pranay</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <section className="hero">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">Pranay</h1>
+              <h2 className="subtitle">Software Developer & Data Scientist</h2>
+            </div> 
+          </div>
+
+        </section>
+        <section className="section">
+            <div className="container">
+              <p>I blog about data science</p>
+            </div> 
+        </section>
       </div>
     );
   }
